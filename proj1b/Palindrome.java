23e4478f74386f297c2e6b6d9a13bbe44645db5b
aijorevue <qinnovation013023@163.com>
2026-02-01 13:05:48 +0800

    第四次提交

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,7 +1,7 @@
 public class Palindrome{
     //the take2
 
-    OffByOne obo=new OffByOne();
+    private OffByOne obo=new OffByOne();
     public Deque<Character> wordToDeque(String word){
         Deque<Character> temp=new ArrayDeque<>();
         for(int i=0;i<word.length();i++){
@@ -10,6 +10,9 @@ public class Palindrome{
         return temp;
     }
     public boolean isPalindrome(String word){
+        if(word.length()==1){
+            return true;
+        }
         for(int i=0;i<word.length();i++){
             char a=word.charAt(i);
             char b=word.charAt(word.length()-i-1);
@@ -20,7 +23,7 @@ public class Palindrome{
         }
         return true;
     }
-    public static boolean isPalindrome(String word, CharacterComparator cc) {
+    public boolean isPalindrome(String word, CharacterComparator cc) {
         int len = word.length();
         for (int i = 0; i < len / 2; i++) {
             if (!cc.equalChars(word.charAt(i), word.charAt(len - 1 - i))) {

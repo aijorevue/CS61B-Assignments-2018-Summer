e3339ad903da004be3768e162322fb4b46296d25
aijorevue <qinnovation013023@163.com>
2026-02-05 22:18:14 +0800

    完成了最简单的Hex

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,57 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static TETile[][] world;
+    private static TERenderer ter=new TERenderer();
+
+    public static void addHexagon(int x,int y,int s,TETile tile){
+        if(s<2){
+            return ;
+        }
+
+        for(int row=0;row<2*s;row++){
+            int rowWidth=calculateRowWidth(s,row);
+            int rowOffset=calculateRowOffset(s,row);
+
+            for(int i=0;i<rowWidth;i++){
+                world[x+rowOffset+i][y+row]=tile;
+            }
+        }
+    }
+
+    private static int calculateRowWidth(int s,int row){
+        if(row<s){
+            return s+2*row;
+        }
+        else{
+            return s+2*(2*s-1-row);
+        }
+    }
+
+    private static int calculateRowOffset(int s,int row){
+        if(row<s){
+            return -(row);
+        }
+        else{
+            return -(2*s-1-row);
+        }
+    }
+
+    public static void main(String [] args){
+        int width=50;
+        int height=50;
+        ter.initialize(width,height);
+
+        world=new TETile[width][height];
+        for(int x=0;x<width;x++){
+            for(int y=0;y<height;y++){
+                world[x][y]=Tileset.NOTHING;
+            }
+        }
+
+        addHexagon(10,10,3,Tileset.GRASS);
+
+        ter.renderFrame(world);
+    }
+
 }

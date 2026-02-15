cc1960f23d749aae17447b39ed7ba911d8f4806b
aijorevue <qinnovation013023@163.com>
2026-02-15 22:31:30 +0800

    第一次提交

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,83 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF whetherPercolation;
+    private WeightedQuickUnionUF whetherFull;
 
+    private boolean[][] opened;
+    private int openSites;
+
+    private int n;
+    private int virtualTop;
+    private int virtualBottom;
+
+    public Percolation(int N) {
+        n = N;
+        virtualTop = N * N;
+        virtualBottom = N * N + 1;
+        whetherPercolation = new WeightedQuickUnionUF(N * N + 2);
+        whetherFull = new WeightedQuickUnionUF(N * N + 1);
+        opened = new boolean[N][N];
+        openSites = 0;
+    }
+
+    private void validate(int row,int col){
+        if(row<0||col<0||row>n-1||col>n-1){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+    }
+
+    private int turn(int row, int col) {
+        return row * n + col;
+    }
+
+    public void open(int row, int col) {
+        validate(row,col);
+        if(isOpen(row,col)){
+            return ;
+        }
+        opened[row][col] = true;
+        openSites++;
+        int current = turn(row, col);
+        if (row == 0) {
+            whetherPercolation.union(current, virtualTop);
+            whetherFull.union(current, virtualTop);
+        }
+        if (row == n - 1) {
+            whetherPercolation.union(current, virtualBottom);
+        }
+        int[] ro = {-1, 1, 0, 0};
+        int[] co = {0, 0, -1, 1};
+        for (int i = 0; i < 4; i++) {
+            int roo = ro[i] + row;
+            int coo = co[i] + col;
+            if (roo >= 0 && coo >= 0 && roo < n && coo < n && isOpen(roo, coo)) {
+                whetherPercolation.union(current, turn(roo, coo));
+                whetherFull.union(current, turn(roo, coo));
+            }
+        }
+    }      // open the site (row, col) if it is not open already
+
+    public boolean isOpen(int row, int col) {
+        validate(row,col);
+        return opened[row][col];
+    }  // is the site (row, col) open?
+
+    public boolean isFull(int row, int col) {
+        validate(row,col);
+        return whetherFull.connected(turn(row, col), virtualTop);
+        //2时代表水灌进来
+    }  // is the site (row, col) full?
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }         // number of open sites
+
+    public boolean percolates() {
+        return whetherPercolation.connected(virtualTop, virtualBottom);
+    }             // does the system percolate?
+
+    public static void main(String[] args) {
+
+    }  // use for unit testing (not required)
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,60 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
 
+import java.util.Random;
+
+public class PercolationStats {
+    private int N;
+    private int T;
+    private PercolationFactory pf;
+
+
+    private double[] counts;
+
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        validate(N, T);
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        this.counts = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            while (!p.percolates()) {
+                int a = StdRandom.uniform(N);
+                int b = StdRandom.uniform(N);
+                if (!p.isOpen(a, b)) {
+                    p.open(a, b);
+                }
+            }
+            counts[i]= (double) p.numberOfOpenSites() /Math.pow(N,2);
+        }
+    }// perform T independent experiments on an N-by-N grid
+    public double mean(){
+        double mean=0;
+        for (double count : counts) {
+            mean += count;
+        }
+        return mean /counts.length;
+    }                                           // sample mean of percolation threshold
+    public double stddev(){
+        double moecule=0;
+        for (double count : counts) {
+            moecule += StrictMath.pow(count - mean(), 2);
+        }
+        return Math.sqrt(moecule/(T-1));
+    }                                 // sample standard deviation of percolation threshold
+    public double confidenceLow(){
+        return mean()-(1.96*stddev())/Math.sqrt(T);
+    }                                  // low endpoint of 95% confidence interval
+    public double confidenceHigh(){
+        return mean()+(1.96*stddev())/Math.sqrt(T);
+    }
+
+    private void validate(int N,int T){
+        if(N<=0||T<=0){
+            throw new IllegalArgumentException("Illegal Argument Exception");
+        }
+    }
 }

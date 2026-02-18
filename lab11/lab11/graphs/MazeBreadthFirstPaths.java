7b4d3698f74c8f59a56d4d642bbcbbd58a928d38
aijorevue <qinnovation013023@163.com>
2026-02-18 16:26:33 +0800

    第三次提交

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+
 /**
  *  @author Josh Hug
+ * @author Garrison
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +12,59 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        this.s = maze.xyTo1D(sourceX, sourceY);
+        this.t = maze.xyTo1D(targetX, targetY);
+        this.distTo[s] = 0;
+        this.edgeTo[s] = s;
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        ArrayDeque<Integer> bfsQueue = new ArrayDeque<>();
+        bfsQueue.addLast(s);
+
+        int curr;
+
+        while (!bfsQueue.isEmpty()) {
+
+            curr = bfsQueue.removeFirst();
+            marked[curr] = true;
+
+            for (int next : maze.adj(curr)) {
+                if (!marked[next]) {
+                    edgeTo[next] = curr;
+                    distTo[next] = distTo[curr] + 1;
+                    announce();
+                    bfsQueue.addLast(next);
+                    if (targetFound) {
+                        return;
+                    }
+                }
+            }
+
+            if (curr == t) {
+                targetFound = true;
+            }
+            if (targetFound) {
+                return;
+            }
+
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
+
 
